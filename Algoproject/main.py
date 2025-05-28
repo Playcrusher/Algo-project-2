2b3c6f2010e749c2fd777347fcd03b1c38ec49e8
Playcrusher <136068475+Playcrusher@users.noreply.github.com>
2025-05-28 19:56:54 +0700

    Add files via upload

---
+++ Algoproject/main.py
@@ -0,0 +1,49 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QRadioButton
+from random import randint
+
+app = QApplication([])
+
+main_win = QWidget()
+main_win.setWindowTitle('Quiz')
+main_win.move(900, 400)
+main_win.resize(400, 200)
+
+question = QLabel('Pada tahun berapa Algorithmics didirikan?')
+btn_answer1 = QRadioButton('2013')
+btn_answer2 = QRadioButton('2010')
+btn_answer3 = QRadioButton('2016')
+btn_answer4 = QRadioButton('2019')
+
+layoutH1 = QHBoxLayout()
+layoutH2 = QHBoxLayout()
+layoutH3 = QHBoxLayout()
+layoutH1.addWidget(question, alignment = Qt.AlignCenter)
+layoutH2.addWidget(btn_answer1, alignment = Qt.AlignCenter)
+layoutH2.addWidget(btn_answer2, alignment = Qt.AlignCenter)
+layoutH3.addWidget(btn_answer3, alignment = Qt.AlignCenter)
+layoutH3.addWidget(btn_answer4, alignment = Qt.AlignCenter)
+
+layout_main = QVBoxLayout()
+layout_main.addLayout(layoutH1)
+layout_main.addLayout(layoutH2)
+layout_main.addLayout(layoutH3)
+main_win.setLayout(layout_main)
+
+def show_win():
+    victory_win = QMessageBox()
+    victory_win.setText('Benar')
+    victory_win.exec_()
+
+def show_lose():
+    lose = QMessageBox()
+    lose.setText('Coba lagi!')
+    lose.exec_()
+
+btn_answer1.clicked.connect(show_lose)
+btn_answer2.clicked.connect(show_lose)
+btn_answer3.clicked.connect(show_win)
+btn_answer4.clicked.connect(show_lose)
+
+main_win.show()
+app.exec_()
